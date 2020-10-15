655015c81c331970e652e426c3499a95b9587d44
Vladyslav Nekriach <ukitta555@gmail.com>
2020-10-15 19:32:15 -0100

    implemented adding rows and columns

--- ExcelLent/Form1.Designer.cs
+++ ExcelLent/Form1.Designer.cs
@@ -28,37 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.FileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Menu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuItem});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(1275, 28);
+            this.Menu.TabIndex = 1;
+            this.Menu.Text = "File";
+            // 
+            // FileMenuItem
+            // 
+            this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Open,
+            this.SaveMenuItem});
+            this.FileMenuItem.Name = "FileMenuItem";
+            this.FileMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.FileMenuItem.Text = "File";
+            // 
+            // Open
+            // 
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(190, 26);
+            this.Open.Text = "Open";
+            // 
+            // SaveMenuItem
+            // 
+            this.SaveMenuItem.Name = "SaveMenuItem";
+            this.SaveMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.SaveMenuItem.Text = "SaveMenuItem";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 24);
+            this.toolStripMenuItem1.Text = "File";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(2, 74);
+            this.dataGridView1.Location = new System.Drawing.Point(2, 27);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(799, 377);
+            this.dataGridView1.Size = new System.Drawing.Size(1261, 424);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.Text = "dataGridView1";
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.MouseHover += new System.EventHandler(this.dataGridView1_MouseHover);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1275, 450);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Menu);
+            this.MainMenuStrip = this.Menu;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Open;
+        private System.Windows.Forms.ToolStripMenuItem SaveMenuItem;
     }
 }
 
--- ExcelLent/Form1.cs
+++ ExcelLent/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,20 +16,74 @@ namespace ExcelLent
         public Form1()
         {
             InitializeComponent();
-            dataGridView1.ColumnCount = 4;
-            dataGridView1.RowCount = 4;
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.RowCount = 5;
             dataGridView1.Columns[0].Name = "Product ID";
             dataGridView1.Columns[1].Name = "Product Name";
             dataGridView1.Columns[2].Name = "Product Price";
             this.dataGridView1.Rows.Insert(0, "one", "two", "three", "four");
             this.dataGridView1.Rows.Insert(0, "Влад", "не ", "любит", "шарп");
             this.dataGridView1.Rows.Add(new string[] { "xd", "lmao" });
+
+            
+
+
+            //btn.UseColumnTextForButtonValue = true;
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_insertRow(int index)
         {
+            //dataGridView1.RowCount++;
+            dataGridView1.Rows.Insert(index);
+        }
 
-            Console.WriteLine("xd");
+        private void dataGridView1_insertCol(int index)
+        {
+            // dataGridView1.ColumnCount++;
+           //DataGridViewCell tmp_cell = new DataGridViewCell();
+           
+           DataGridViewColumn columnToInsert = new DataGridViewColumn();
+
+           columnToInsert.CellTemplate = new DataGridViewTextBoxCell();
+
+           dataGridView1.Columns.Insert(index, columnToInsert);
+           dataGridView1.Columns[index].Name = "New Column";
+           dataGridView1.Columns[index].SortMode = DataGridViewColumnSortMode.Automatic;
+           dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex == dataGridView1.RowCount - 1)
+                {
+                    dataGridView1_insertCol(e.ColumnIndex + 1);
+                }
+                else if (e.ColumnIndex == -1)
+                {   
+                    dataGridView1_insertRow(e.RowIndex + 1);
+                    
+                }
+            }
+            catch (IndexOutOfRangeException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void dataGridView1_MouseHover(object sender, EventArgs e)
+        {
+            var p = dataGridView1.PointToClient(Cursor.Position);
+            var info = dataGridView1.HitTest(p.X, p.Y);
+
+            if (info.RowIndex == 0)
+            {
+                //object value = dataGridView1.Rows[0].Cells[info.ColumnIndex].Value;
+                //dataGridView1.Columns[info.ColumnIndex].CellTemplate = new DataGridViewHeader
+            }
+            //MessageBox.Show(dataGridView1.Columns[0].HeaderCell.GetType().ToString());
+            //MessageBox.Show(dataGridView1[2, -1].GetType().ToString());
         }
     }
 }
